d4e64551067ddc2afc36f80cdfad494619f6334f
Cristóbal Berríos <crisberrios@uc.cl>
2024-06-04 20:32:57 -0400

    router setup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="flex flex-row justify-around">
-    <h1 class="text-3xl font-bold underline">Outdoor Life</h1>
-  </div>
+  <nav>
+    <RouterLink to="/">Go to Home</RouterLink>
+    <RouterLink to="/about">Go to About</RouterLink>
+  </nav>
+  <RouterView />
 </template>
 <script></script>
---
+++ src/pages/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="flex flex-row justify-around">
+    <h1 class="text-3xl font-bold underline">Outdoor Life</h1>
+  </div>
+</template>
