27ea926d99c174b080b55bce53e2ed5d37ce5c16
Cristóbal Berríos <crisberrios@uc.cl>
2024-06-06 19:28:22 -0400

    about page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>About</h1>
+  <div class="content">
+    <h1 class="font-serif text-3xl">About</h1>
+    <p>
+      This page uses information from
+      <a class="link" href="https://www.nps.gov/subjects/developer/api-documentation.htm#">this api</a>
+      to display information about parks.
+    </p>
+  </div>
 </template>
+
+<style scoped>
+.link {
+  color: blue;
+  text-decoration: underline;
+}
+</style>
