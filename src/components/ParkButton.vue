8a3257f101f67fc36d75c2753b0ce687df27a3e7
Cristóbal Berríos <crisberrios@uc.cl>
2024-06-06 11:05:57 -0400

    thumbnail in park button

--- src/components/ParkButton.vue
+++ src/components/ParkButton.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="park-button" :key="park.key">{{ park.name }}</div>
+  <div class="park-button" :key="park.key">
+    <img :src="park.images[0].url" class="park-thumbnail m-2" alt="Park Thumbnail" />
+    <p>{{ park.fullName }}</p>
+  </div>
 </template>
 
 <script setup>
@@ -16,6 +19,15 @@ const props = defineProps({
   margin: 5px;
   padding: 10px;
   text-align: center;
-  width: 200px;
+  width: 40%;
+  max-height: 20vh;
+  display: flex;
+  align-items: center;
+}
+
+.park-thumbnail {
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
 }
 </style>
